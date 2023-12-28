2be3417bf77054c49ffe75bf1e8e7e47ecdb5a89
Ebinesh <ebinesh2511@gmail.com>
2023-12-28 08:05:14 +0530

    Created Devise model for user admin head admin, configuring ticket model, established associations

---
+++ app/models/admin.rb
@@ -0,0 +1,9 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :tickets, dependent: :destroy, foreign_key: 'admin_id'
+
+end
